3e6060bda6e6368b7020305d99b2385983382762
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-10-08 07:26:36 +0530

    Update Main.js

--- EmersonTest/scripts/Main.js
+++ EmersonTest/scripts/Main.js
@@ -42,29 +42,29 @@ define("EmersonTest/scripts/Main", [
         </div>`;
 
         let droppableContainer = document.getElementById("droppableContainer");
-        DataDragAndDrop.droppable(droppableContainer, {
-            drop: function (data) {
-                console.log("data", data)
-            droppableContainer.classList.add("drag-over");
+        // DataDragAndDrop.droppable(droppableContainer, {
+        //     drop: function (data) {
+        //         console.log("data", data)
+        //     droppableContainer.classList.add("drag-over");
              
-              var dropedObject = JSON.parse(data);
+        //       var dropedObject = JSON.parse(data);
   
-            //   var objId = dropedObject.data["items"][0].objectId;
-            //   that.objectId = objId;
-            //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
-            //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
-            //   that.isBtnCAReportDisabled = false;
-            //   that.callAllMethods();
-            },
-            enter: function () {
-                console.log("Enter");
-                    droppableContainer.classList.add("drag-over");
-            },
-            leave: function () {
-                console.log("leave");
-                droppableContainer.classList.remove("drag-over");
-            },
-          });
+        //     //   var objId = dropedObject.data["items"][0].objectId;
+        //     //   that.objectId = objId;
+        //     //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
+        //     //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
+        //     //   that.isBtnCAReportDisabled = false;
+        //     //   that.callAllMethods();
+        //     },
+        //     enter: function () {
+        //         console.log("Enter");
+        //             droppableContainer.classList.add("drag-over");
+        //     },
+        //     leave: function () {
+        //         console.log("leave");
+        //         droppableContainer.classList.remove("drag-over");
+        //     },
+        //   });
 
         widget.body.innerHTML = temp;
 
@@ -72,6 +72,31 @@ define("EmersonTest/scripts/Main", [
             },
             updateWidget: function () {
                 alert("In updateWidget");
+
+                var temp =
+                    `<div id="droppableContainer" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; color: blue;">
+            <img 
+                src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/drag-and-drop.png" 
+                alt="Drag and Drop" 
+                style="width: 60px; height: 60px;" 
+            />
+            <span style="font-size: 20px; color: black;">Drag and Drop</span>
+            <div style="display: flex; align-items: center; margin-top: 20px; width: 30%;">
+                <hr style="flex: 1;" />
+                <span style="margin: 0 10px; color: black;">or</span>
+                <hr style="flex: 1;" />
+            </div>
+            <div style="display: flex; align-items: center; margin-top: 20px;">
+                <img 
+                    src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/I_WI_DataCollect108x144.png" 
+                    alt="Data Collect" 
+                    style="width: 60px; height: 50px; margin-right: 10px;" 
+                />
+                <span style="font-size: 20px; color: black;">Click here to search content</span>
+            </div>
+        </div>`;
+
+        let droppableContainer = document.getElementById("droppableContainer");
             },
             // getData: function () {
             //     let spaceURL = "https://3dxr21x-d4.emrsn.org:447/3dspace";
