22fa9dea861380b56ba398e5433b96288962401e
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-10-08 06:28:56 +0530

    Update Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -6,15 +6,17 @@
 define("EmersonTest/scripts/Main", [
     "DS/PlatformAPI/PlatformAPI",
     "DS/WAFData/WAFData",
+    "EmersonTest/components/dragAndDrop"
 ],
-    function (PlatformAPI, WAFData) {
+    function (PlatformAPI, WAFData,dragAndDrop) {
 
         var myWidget = {
             ObjectId: "",
             name: "Amit",
             onLoad: function () {
-                alert("In ON load");
-               
+                alert("In ON load 2");
+                alert(widget.dragAndDropComp.showDroppable());
+                
 
 
             },
