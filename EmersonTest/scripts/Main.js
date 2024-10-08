c5e933d513032bfc0419b51e0cfe3a087acd86b0
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-10-08 08:37:40 +0530

    Update Main.js

--- EmersonTest/scripts/Main.js
+++ EmersonTest/scripts/Main.js
@@ -39,37 +39,37 @@ define("EmersonTest/scripts/Main", [
             </div>
         </div>`;
 
-        
-        widget.body.innerHTML = temp;
-        var droppableContainer = widget.body.querySelector('.droppableContainer');        
-        debugger;
-        DataDragAndDroplib.droppable(droppableContainer, {
-            drop: function (data) {
-                console.log("data", data)
-                droppableContainer.classList.remove("drag-over");
-             
-              var dropedObject = JSON.parse(data);
-              myWidget.getDroppedObjectInfo(dropedObject.data.items) ;
-              
-  
-            //   var objId = dropedObject.data["items"][0].objectId;
-            //   that.objectId = objId;
-            //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
-            //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
-            //   that.isBtnCAReportDisabled = false;
-            //   that.callAllMethods();
-            },
-            enter: function () {
-                console.log("Enter");
-                    droppableContainer.classList.add("drag-over");
-            },
-            leave: function () {
-                console.log("leave");
-                droppableContainer.classList.remove("drag-over");
-            },
-          });
 
-        
+                widget.body.innerHTML = temp;
+                var droppableContainer = widget.body.querySelector('.droppableContainer');
+                debugger;
+                DataDragAndDroplib.droppable(droppableContainer, {
+                    drop: function (data) {
+                        console.log("data", data)
+                        droppableContainer.classList.remove("drag-over");
+
+                        var dropedObject = JSON.parse(data);
+                        myWidget.getDroppedObjectInfo(dropedObject.data.items);
+
+
+                        //   var objId = dropedObject.data["items"][0].objectId;
+                        //   that.objectId = objId;
+                        //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
+                        //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
+                        //   that.isBtnCAReportDisabled = false;
+                        //   that.callAllMethods();
+                    },
+                    enter: function () {
+                        console.log("Enter");
+                        droppableContainer.classList.add("drag-over");
+                    },
+                    leave: function () {
+                        console.log("leave");
+                        droppableContainer.classList.remove("drag-over");
+                    },
+                });
+
+
 
 
             },
@@ -99,34 +99,34 @@ define("EmersonTest/scripts/Main", [
             </div>
         </div>`;
 
-         widget.body.innerHTML = temp;
-        var droppableContainer = widget.body.querySelector('.droppableContainer');
+                widget.body.innerHTML = temp;
+                var droppableContainer = widget.body.querySelector('.droppableContainer');
                 DataDragAndDroplib.droppable(droppableContainer, {
-            drop: function (data) {
-                console.log("data", data)
-            droppableContainer.classList.add("drag-over");
-             
-              
-              var dropedObject = JSON.parse(data);
-              myWidget.getDroppedObjectInfo(dropedObject.data.items) ;
-  
-            //   var objId = dropedObject.data["items"][0].objectId;
-            //   that.objectId = objId;
-            //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
-            //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
-            //   that.isBtnCAReportDisabled = false;
-            //   that.callAllMethods();
-            },
-            enter: function () {
-                console.log("Enter");
-                    droppableContainer.classList.add("drag-over");
-            },
-            leave: function () {
-                console.log("leave");
-                droppableContainer.classList.remove("drag-over");
-            },
-          });
-        debugger;
+                    drop: function (data) {
+                        console.log("data", data)
+                        droppableContainer.classList.add("drag-over");
+
+
+                        var dropedObject = JSON.parse(data);
+                        myWidget.getDroppedObjectInfo(dropedObject.data.items);
+
+                        //   var objId = dropedObject.data["items"][0].objectId;
+                        //   that.objectId = objId;
+                        //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
+                        //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
+                        //   that.isBtnCAReportDisabled = false;
+                        //   that.callAllMethods();
+                    },
+                    enter: function () {
+                        console.log("Enter");
+                        droppableContainer.classList.add("drag-over");
+                    },
+                    leave: function () {
+                        console.log("leave");
+                        droppableContainer.classList.remove("drag-over");
+                    },
+                });
+                debugger;
             }, getDroppedObjectInfo: function (data) {
                 if (data.length > 1) {
                     alert("Please drop only one object");
@@ -134,8 +134,7 @@ define("EmersonTest/scripts/Main", [
                 } else {
                     getCSRFToken(data);
                 }
-            }, getCSRFToken: function(data) {
-
+            }, getCSRFToken: function (data) {
                 // URLs
                 let csrfURL = "https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/application/CSRF?tenant=OI000186152"
                 let finalURL = "https://r1132101608061-usw1-space.3dexperience.3ds.com/enovia//resources/v1/modeler/dseng/dseng:EngItem/";
@@ -151,7 +150,6 @@ define("EmersonTest/scripts/Main", [
                     timeout: 150000,
                     type: "json",
                     onComplete: function (dataResp2, headerResp2) {
-
                         const csrfToken = dataResp2.csrf.name;
                         const csrfValue = dataResp2.csrf.value;
                         const securityContextHeader = 'SecurityContext';
@@ -162,7 +160,7 @@ define("EmersonTest/scripts/Main", [
                         myHeaders[securityContextHeader] = securityContextValue;
 
                         finalURL += data.objectID;
-                        console.log("finalURL",finalURL);
+                        console.log("finalURL", finalURL);
                         WAFData.authenticatedRequest(finalURL, {
                             method: "Get",
                             headers: myHeaders,
@@ -171,65 +169,19 @@ define("EmersonTest/scripts/Main", [
                             timeout: 150000,
                             type: "json",
                             onComplete: function (dataResp3, headerResp3) {
-                                alert(dataResp3);
-                                // let changeActionList = dataResp3.changeAction;
-                                // for (let changeActionCount = 0; changeActionCount < changeActionList.length; changeActionCount++) {
-                                //     changeAction = changeActionList[changeActionCount];
-                                //     let source = changeAction.source;
-                                //     let relativePathUrl = changeAction.relativePath;
-
-
-                                //     let caPropURL = source + relativePathUrl;
-
-                                    // WAFData.authenticatedRequest(caPropURL, {
-                                    //     method: "Get",
-                                    //     headers: myHeaders,
-                                    //     data: {
-
-                                    //     },
-                                    //     timeout: 150000,
-                                    //     type: "json",
-                                    //     onComplete: function (dataResp4, headerResp4) {
-                                    //         let caTitle = dataResp4.title;
-                                    //         bodyhtml += "<div class='grid-items' style='font-size: small;background-color: #FFFBDF;padding: 10px;'>";
-                                    //         bodyhtml += "<div><b>Title :</b> " + caTitle + "</div>";
-                                    //         bodyhtml += "<div><b>Name :</b> " + dataResp4.name + "</div>";
-                                    //         bodyhtml += "<div><b>Owner :</b> " + dataResp4.owner + "</div>";
-                                    //         bodyhtml += "<div><b>Collab Space :</b> " + dataResp4.collabSpace + "</div>";
-                                    //         bodyhtml += "</div>";
-
-                                    //         if (changeActionCount == changeActionList.length - 1) {
-                                    //             bodyhtml += "</div>"
-                                    //             widget.body.innerHTML = bodyhtml;
-                                    //         }
-
-                                    //     },
-                                    //     onFailure: function (error4, responseDOMString4, headerResp4) {
-                                    //         widget.bodyhtml.innerHTML = "Error : "+error4;
-                                    //         debugger;
-                                    //     }
-                                    // });
-
-                                }
-
-
-
-                            },
-                            onFailure: function (error3, responseDOMString3, headerResp3) {
-                                widget.bodyhtml.innerHTML = "Error : "+error3;
-                                debugger;
-
+                                console.log("dataResp3",dataResp3);
                             }
                         });
-                    },
-                    onFailure: function (error2, responseDOMString2, headerResp2) {
-                        widget.bodyhtml.innerHTML = "Error : "+error2;
-                        debugger;
 
                     }
                 });
-
             }
+
+
+
+
+
+
             // getData: function () {
             //     let spaceURL = "https://3dxr21x-d4.emrsn.org:447/3dspace";
             //     let urlWAF = spaceURL + "/EmersonTestModel/EmersonTestService/getTestData";
