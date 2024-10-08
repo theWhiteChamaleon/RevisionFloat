ee5ad1ee16b97f8c71f9e80c80b5cf257df899a5
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-10-08 15:31:31 +0530

    Update Main.js

--- EmersonTest/scripts/Main.js
+++ EmersonTest/scripts/Main.js
@@ -6,68 +6,70 @@
 define("EmersonTest/scripts/Main", [
     "DS/DataDragAndDrop/DataDragAndDrop",
     "DS/WAFData/WAFData",
+    "EmersonTest/components/dragAndDrop",
     "css!EmersonTest/styles/revstyles.css"
 ],
-    function (DataDragAndDroplib, WAFData) {
+    function (DataDragAndDroplib, WAFData,dragAndDropComp) {
 
         var myWidget = {
-            ObjectId: "",
-            name: "Amit",
+            
             onLoad: function () {
-                alert("In ON load 2");
-
-                var temp =
-                    `<div class="droppableContainer" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; color: blue;">
-            <img 
-                src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/drag-and-drop.png" 
-                alt="Drag and Drop" 
-                style="width: 60px; height: 60px;" 
-            />
-            <span style="font-size: 20px; color: black;">Drag and Drop</span>
-            <div style="display: flex; align-items: center; margin-top: 20px; width: 30%;">
-                <hr style="flex: 1;" />
-                <span style="margin: 0 10px; color: black;">or</span>
-                <hr style="flex: 1;" />
-            </div>
-            <div style="display: flex; align-items: center; margin-top: 20px;">
-                <img 
-                    src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/I_WI_DataCollect108x144.png" 
-                    alt="Data Collect" 
-                    style="width: 60px; height: 50px; margin-right: 10px;" 
-                />
-                <span style="font-size: 20px; color: black;">Click here to search content</span>
-            </div>
-        </div>`;
+                alert("In ON load 3");
+                dragAndDropComp.showDroppable();
 
 
-                widget.body.innerHTML = temp;
-                var droppableContainer = widget.body.querySelector('.droppableContainer');
-                debugger;
-                DataDragAndDroplib.droppable(droppableContainer, {
-                    drop: function (data) {
-                        console.log("data", data)
-                        droppableContainer.classList.remove("drag-over");
+        //         var temp =
+        //             `<div class="droppableContainer" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; color: blue;">
+        //     <img 
+        //         src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/drag-and-drop.png" 
+        //         alt="Drag and Drop" 
+        //         style="width: 60px; height: 60px;" 
+        //     />
+        //     <span style="font-size: 20px; color: black;">Drag and Drop</span>
+        //     <div style="display: flex; align-items: center; margin-top: 20px; width: 30%;">
+        //         <hr style="flex: 1;" />
+        //         <span style="margin: 0 10px; color: black;">or</span>
+        //         <hr style="flex: 1;" />
+        //     </div>
+        //     <div style="display: flex; align-items: center; margin-top: 20px;">
+        //         <img 
+        //             src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/I_WI_DataCollect108x144.png" 
+        //             alt="Data Collect" 
+        //             style="width: 60px; height: 50px; margin-right: 10px;" 
+        //         />
+        //         <span style="font-size: 20px; color: black;">Click here to search content</span>
+        //     </div>
+        // </div>`;
 
-                        var dropedObject = JSON.parse(data);
-                        myWidget.getDroppedObjectInfo(dropedObject.data.items);
+
+        //         widget.body.innerHTML = temp;
+        //         var droppableContainer = widget.body.querySelector('.droppableContainer');
+        //         debugger;
+        //         DataDragAndDroplib.droppable(droppableContainer, {
+        //             drop: function (data) {
+        //                 console.log("data", data)
+        //                 droppableContainer.classList.remove("drag-over");
+
+        //                 var dropedObject = JSON.parse(data);
+        //                 myWidget.getDroppedObjectInfo(dropedObject.data.items);
 
 
-                        //   var objId = dropedObject.data["items"][0].objectId;
-                        //   that.objectId = objId;
-                        //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
-                        //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
-                        //   that.isBtnCAReportDisabled = false;
-                        //   that.callAllMethods();
-                    },
-                    enter: function () {
-                        console.log("Enter");
-                        droppableContainer.classList.add("drag-over");
-                    },
-                    leave: function () {
-                        console.log("leave");
-                        droppableContainer.classList.remove("drag-over");
-                    },
-                });
+        //                 //   var objId = dropedObject.data["items"][0].objectId;
+        //                 //   that.objectId = objId;
+        //                 //   PlatformAPI.publish("DropRCAID", that.objectId) //ZSIAHBH : PLMRM-9640 Refresh - Sync
+        //                 //   that.dropCADisplayName = dropedObject.data["items"][0].displayName;
+        //                 //   that.isBtnCAReportDisabled = false;
+        //                 //   that.callAllMethods();
+        //             },
+        //             enter: function () {
+        //                 console.log("Enter");
+        //                 droppableContainer.classList.add("drag-over");
+        //             },
+        //             leave: function () {
+        //                 console.log("leave");
+        //                 droppableContainer.classList.remove("drag-over");
+        //             },
+        //         });
 
 
 
@@ -137,7 +139,7 @@ define("EmersonTest/scripts/Main", [
             }, getCSRFToken: function (data) {
                 // URLs
                 let csrfURL = "https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/application/CSRF?tenant=OI000186152"
-                let finalURL = "https://r1132101608061-usw1-space.3dexperience.3ds.com/enovia//resources/v1/modeler/dseng/dseng:EngItem/";
+                let finalURL = "https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/modeler/dseng/dseng:EngItem/";
 
                 WAFData.proxifiedRequest(csrfURL, {
                     method: "Get",
