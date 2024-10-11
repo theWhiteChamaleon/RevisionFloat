71a10f140523336914f9a4aa9709eb67f5157c3e
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-10-11 14:00:27 +0530

    Update table.js

--- EmersonTest/components/table.js
+++ EmersonTest/components/table.js
@@ -3,46 +3,35 @@ define("EmersonTest/components/table", ["DS/DataDragAndDrop/DataDragAndDrop", "D
     var whereUsedTable = {
         showTable: function (data) {
            
-
-            var tableHTML = `
-                <div class="card-container">
-                    <div class="row card-boundary">
-                        <div class="col-sm-2">
-                                <div class="card-image" style="flex: 1;">
-                                    <img src="https://thewhitechamaleon.github.io/RevisionFloat/EmersonTest/assets/images/PhysicalProductLarge.png" alt="Sample Image" style="width: 100%;">
-                                </div></div>
-                                <div class="col-sm-10" style="flex: 1;">
-                                    <div class="card-content">
-            `;
-
            
+        // Create Tabulator table
+        var table = new Tabulator("#example-table", {
+            data: data, // Assign data to table
+            autoColumns: true, // Create columns from data keys
+            layout: "fitColumns", // Fit columns to width of table
+            movableColumns: true, // Allow column order to be changed
+            selectable: true, // Enable row selection
+            columns: [
+                {formatter: "rowSelection", titleFormatter: "rowSelection", hozAlign: "center", headerSort: false, cellClick: function(e, cell){
+                    cell.getRow().toggleSelect();
+                }},
+                // Additional columns can be defined here if needed
+            ],
+            pagination: "local", // Enable local pagination
+            paginationSize: 10, // Number of rows per page
+            paginationSizeSelector: [10, 20, 30, 40], // Page size options
+            initialSort: [ // Define initial sort order
+                {column: "name", dir: "asc"} // Sort by "name" column in ascending order
+            ],
+            headerFilterPlaceholder: "Search...", // Placeholder for search input
+            headerFilterLiveFilter: true, // Enable live filtering
+        });
 
+        // Append table to DOM
+        document.getElementById("table-container").appendChild(table.element);
 
-            widget.body.insertAdjacentHTML('beforeend', tableHTML);
+        
            
-        }, getAllRevisions: function (data) {
-
-            let finalURL = "https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/modeler/dslc/version/getGraph";
-            WAFData.authenticatedRequest(finalURL, {
-                method: "Post",
-                headers: droppableContainer.csrfHeaders,
-                data: {
-                    "data": [
-                      {
-                        "id": data.objectId,
-                        "identifier": data.objectId,
-                        "type": data.type,
-                        "source": "https://oi000186152-us1-space.3dexperience.3ds.com/enovia",
-                        "relativePath": "/resources/v1/modeler/dseng/dseng:EngItem/"+data.objectId
-                      }
-                    ]
-                  },
-                timeout: 150000,
-                type: "json",
-                onComplete: function (dataResp3, headerResp2) {
-                    console.log("dataResp2", dataResp2);
-                }
-            });
         }
     }
     widget.whereUsedTable = whereUsedTable;
