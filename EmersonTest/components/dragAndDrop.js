5c82dc3479bf186f42499d8ac78a71494f9d7b48
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-11-07 11:50:45 +0530

    Update dragAndDrop.js

--- EmersonTest/components/dragAndDrop.js
+++ EmersonTest/components/dragAndDrop.js
@@ -82,7 +82,7 @@ define("EmersonTest/components/dragAndDrop", ["DS/DataDragAndDrop/DataDragAndDro
                 let finalURL = "https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/modeler/dseng/dseng:EngItem/";
                 let cadSearchURL = "https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/modeler/dsxcad/dsxcad:Product/search";
 
-                WAFData.proxifiedRequest(csrfURL, {
+                WAFData.authenticatedRequest(csrfURL, {
                     method: "Get",
                     headers: {
 
