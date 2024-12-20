05da331c6b23d297177be93adb9718aac2972a08
Amit Sonje <78496960+theWhiteChamaleon@users.noreply.github.com>
2024-12-20 11:28:11 +0530

    Update tableToolbar.js

--- EmersonTest/components/tableToolbar.js
+++ EmersonTest/components/tableToolbar.js
@@ -48,6 +48,68 @@ define("EmersonTest/components/tableToolbar", ["DS/DataDragAndDrop/DataDragAndDr
         },deselectAll: function (tableData) {
             widget.whereUsedTable.tableData.deselectRow();
         },selectMyCollbSpaceObjs: function (tableData) {
+
+            console.log("---In Search---")
+            count=0;
+       let precond = "";
+       //add types here
+         precond= 'flattenedtaxonomies:"types/VPMReference"';
+         //precond+="NOT (physicalid:06E3A8E2C40800001EA56E5A1F8B0500)";
+         var i;
+         var h = Core.Utils.getUUID();
+         var g = this;
+         g.is3DSearchActive = true;
+         if (!Core.is(i)) {
+           require(["DS/SNInfraUX/SearchCom"], function (k) {
+             i = k.createSocket({
+                 socket_id: h
+               });
+             var l = {
+               title:"Products" ,
+               //title: "Project Space",
+               role: "",
+               mode: "furtive",
+               default_with_precond: true,
+               precond: precond,
+               //precond: 'flattenedtaxonomies:"types/Project Space"',
+               //precond: 'flattenedtaxonomies:"types/Route Template" AND current:"active" AND latestrevision:"TRUE"',
+               show_precond: false,
+               multiSel: false,
+               idcard_activated: false,
+               select_result_max_idcard: false,
+               itemViewClickHandler: "",
+               app_socket_id: h,
+               widget_id: h,
+               search_delegation: "3dsearch",
+               default_search_criteria: ""
+             };
+             if (widget.getPreference("collab-storage") != undefined) {
+               l.tenant = widget.getPreference("collab-storage").value
+             } else {
+                l.tenant = "OI000186152";
+             }
+             var j =["VPMReference"]; //addd types here
+             l.recent_search = {
+               types: j
+             };
+             l.app_socket_id = h;
+             l.widget_id = h;
+             l.global_actions = [{
+                 id: "incontextHelp",
+                 title: "Help",
+                 icon: "fonticon fonticon-help",
+                 overflow: false
+               }
+             ];
+             if (Core.is(i)) {
+               i.dispatchEvent("RegisterContext", l);
+               i.addListener("Selected_Objects_search", g.selected_Search_Objects);
+               i.dispatchEvent("InContextSearch", l)
+             } else {
+               throw new Error("Socket not initialized")
+             }
+           })
+         }
             
         }, replaceObjects: function () {
             widget.whereUsedTable.updateAllRevision();
